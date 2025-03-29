98fcceeeceffb96588635a17aac4d72f1e39a9e0
Maksym Avirum <avirum.max@ukr.net>
2025-03-29 21:02:20 +0200

    HT 7 fixed 4 task

--- js/task-4.js
+++ js/task-4.js
@@ -7,6 +7,7 @@ function handleSubmit(event) {
     const elements = event.target.elements;
     if (elements.email.value.trim() == 0 || elements.password.value.trim() == 0) {
         alert('All form fields must be filled in');
+        return;
     }
     const formObject = {
         email: elements.email.value.trim(),
